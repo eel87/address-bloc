6bf8259228b0691325cfc114358451ba848cb302
Erin Leck <erinleck@gmail.com>
2017-09-20 18:02:53 +0000

    chkpt-6-assignment

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -81,6 +81,35 @@ RSpec.describe AddressBook do
         
             check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
         end
+        
+        
+        it "imports the correct number of entries from another file" do
+            book.import_from_csv("entries_2.csv")
+            book_size = book.entries.size
+            
+            expect(book_size).to eq 3
+        end
+        
+        it "imports the 1st entry" do
+            book.import_from_csv("entries_2.csv")
+            entry_one = book.entries[0]
+        
+            check_entry(entry_one, "Daisy", "475-475-5123", "daisy@rabbit.com")
+        end
+        
+        it "imports the 2nd entry" do
+            book.import_from_csv("entries_2.csv")
+            entry_two = book.entries[1]
+        
+            check_entry(entry_two, "Monte", "475-475-4123", "monte@rabbit.com")
+        end
+        
+        it "imports the 3rd entry" do
+            book.import_from_csv("entries_2.csv")
+            entry_three = book.entries[2]
+        
+            check_entry(entry_three, "Rhoda", "566-566-7123", "rhoda@rabbit.com")
+        end
     end
     
 end
